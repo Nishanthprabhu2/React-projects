440524b8e5cfbcf60c13a4423a5648fd09b26ecf
nishanthprabhu2 <nishanthprabhu26@gmail.com>
2021-10-30 19:01:00 +0530

    axios fetch  fro json placeholder

--- hookspractice/src/App.js
+++ hookspractice/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import Useeffecthook from './components/useeffecthook/Useeffecthook';
+import Datafetch from './components/useeffecthook/Datafetch';
+// import Useeffecthook from './components/useeffecthook/Useeffecthook';
 //import Usestatehook from './components/usestatehook/Usestatehook';
 
 function App() {
   return (
     <div className="App">
       {/* <Usestatehook ></Usestatehook> */}
-      <Useeffecthook></Useeffecthook>
+      {/* <Useeffecthook></Useeffecthook> */}
+      <Datafetch />
     </div>
   );
 }
