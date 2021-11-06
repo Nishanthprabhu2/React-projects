043a06dffd99e2d8a7b83d1c0eb7491516c2098e
nishanthprabhu2 <nishanthprabhu26@gmail.com>
2021-11-06 10:19:52 +0530

    initial commit to weather app

--- starter-files/src/components/Page/Page.jsx
+++ starter-files/src/components/Page/Page.jsx
@@ -1,11 +1,23 @@
 import React, { Fragment } from 'react';
-
+import Form from './Form'
+import Error from './Error'
+import Loader from './Loader'
+import Forecast from './Forecast'
 import Header from '../Header';
+
 
 const Page = () => {
     return (
         <Fragment>
             <Header />
+            {/* Form */}
+            <Form />
+            {/* Error */}
+            <Error />
+            {/* Loader */}
+            <Loader />
+            {/* Forecast */}
+            <Forecast />
         </Fragment>
     );
 };
